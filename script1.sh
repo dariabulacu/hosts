46257e72294f6576c1e1a26a2bf0691256326b4a
dariabulacu <daria.bulacu@s.unibuc.ro>
2024-11-17 17:10:28 +0200

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+ip_verificare(){
+nume_host=$1
+ip_nou=$2
+dns_server=$3
+ip_obtinut=$(nslookup $nume_host $dns_server | grep "Address" | tail -n 1 | awk '{print $2}')
+if [[ "$ip_obtinut" != "$ip_nou" ]]; then
+    echo "Bogus IP pentru $nume_host în /etc/hosts!"
+fi 
 
+}
+cat /etc/hosts | while read line; do
+  ip=$(echo $line |cut -d ' ' f1)
+  name=$(echo $line |cut -d ' ' f2)
+ip_verificare( $ip $name "8.8.8.8")
